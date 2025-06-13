f09f9246b3f3306ee9ee46079dc9f787b123469b
Rajesh-770 <rjaws770@gmail.com>
2025-06-13 06:00:42 +0000

    resolved conflict in hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
+<<<<<<< Updated upstream
 
+=======
+def factorial while loop(n)
+ result=1
+ i=1
+  while i<=n:
+  result*=i
+  i+=1
+ return result
+ number=(int(input("enter a no:"))
+ printf("the fact of {number} {factorial_while_loop(number)"})
+>>>>>>> Stashed changes
